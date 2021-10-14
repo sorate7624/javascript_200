7aa5e40ce19019b965a74c6187468c12fb62834c
최혜진 <choehyejin@choehyejin-ui-MacBookAir.local>
2021-10-14 22:55:16 +0900

    let으로 변수 선언하기

--- part2/041.js
+++ part2/041.js
@@ -1,1 +1,7 @@
 // let으로 변수 선언하기
+if(true){
+  var functionScopeValue = 'global';
+  let blockScopeValue = 'local';
+}
+console.log(functionScopeValue); // global
+// console.log(blockScopeValue); // ReferenceError;
