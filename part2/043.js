ded08b5543aa478843195ec8409e091abec63a86
최혜진 <choehyejin@choehyejin-ui-MacBookAir.local>
2021-10-14 22:56:40 +0900

    스코프 체인 이해하기

--- part2/043.js
+++ part2/043.js
@@ -1,1 +1,18 @@
 // 스코프 체인 이해하기
+var person = 'harin';
+
+function print() {
+  var person2 = 'jay';
+
+  function innerPrint() {
+    console.log(person);
+    console.log(person2);
+  }
+
+  innerPrint();
+
+  console.log('print finished')
+}
+
+print();
+console.log('finished');
