05dd937692302b440170e64b300e28cfa65c83c0
최 혜진(Ella) <ella@optolane.com>
2021-10-18 12:35:19 +0900

    자바스크립트 객체지향 프로그래밍 이해하기

--- part2/048.js
+++ part2/048.js
@@ -1,2 +1,21 @@
 // 자바스크립트 객체지향 프로그래밍 이해하기
+const teacherJay = {
+  name: '제이',
+  age: 30,
+  teaceJavascript: function (student) {
+    student.gainExp();
+  }
+}
 
+const studentBbo = {
+  name: '뽀',
+  age: 20,
+  exp: 0,
+  gainExp: function () {
+    this.exp++;
+  }
+}
+
+console.log(studentBbo.exp);
+teacherJay.teaceJavascript(studentBbo);
+console.log(studentBbo.exp);
