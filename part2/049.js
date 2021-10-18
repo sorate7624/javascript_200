b7535496970ee564836e4ca944f51cb607dd4d29
최 혜진(Ella) <ella@optolane.com>
2021-10-18 12:38:06 +0900

    생성자 함수 이해하기

--- part2/049.js
+++ part2/049.js
@@ -1,2 +1,20 @@
 // 생성자 함수 이해하기
+function Teacher(name, age, subject) {
+  this.name = name;
+  this.age = age;
+  this.subject = subject;
+  this.teach = function (student) {
+    console.log(student + '에게' + this.subject + '를 가르칩니다.');
+  }
+}
 
+const jay = new Teacher('jay', 30, 'Javascript');
+console.log(jay);
+jay.teach('bbo');
+
+console.log(jay.constructor);
+console.log(jay instanceof Teacher);
+
+const jay2 = Teacher('jay', 30, 'Javascript');
+console.log(jay2);
+console.log(age);
