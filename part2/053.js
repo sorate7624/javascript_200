4e51526344ab45d63e4e67198343a6e9760310b4
최 혜진(Ella) <ella@optolane.com>
2021-10-19 12:41:15 +0900

    클래스 정적 메소드와 속성 정의하기

--- part2/053.js
+++ part2/053.js
@@ -1,1 +1,30 @@
-// 클래스 정적 메소드와 속서 정의하기
+// 클래스 정적 메소드와 속성 정의하기
+class Product {
+  static build(name, price) {
+    const id = Math.floor(Math.random() * 1000);
+    return new Product(id, name, price);
+  }
+
+  static getTaxPrice(product) {
+    return (product.price * 0.1) + product.price;
+  }
+
+  constructor(id, name, price) {
+    this.id = id;
+    this.nme = name;
+    this.price = price;
+  }
+}
+
+class DeposableProduct extends Product {
+  depose() {
+    this.deposed = true;
+  }
+}
+
+const gum = Product.build('껌', 1000);
+console.log(gum);
+
+const clothes = new DeposableProduct(1, '옷', 2000);
+const taxPrice = DeposableProduct.getTaxPrice(clothes);
+console.log(taxPrice);
