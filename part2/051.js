e0432e54af34668c708ee7d6f47a2d05f1a1cfd8
최 혜진(Ella) <ella@optolane.com>
2021-10-19 12:32:55 +0900

    클래스 이해하기

---
+++ part2/051.js
@@ -0,0 +1,25 @@
+// 클래스 이해하기
+class Cart {
+  constructor() {
+    this.store = {};
+  }
+
+  addProduct(product) {
+    this.store[product.id] = product;
+  }
+
+  getProduct(id) {
+    return this.store[id];
+  }
+}
+
+const cart1 = new Cart();
+
+cart1.addProduct({
+  id: 1,
+  name: '노트북'
+});
+console.log(cart1.store);
+
+const p = cart1.getProduct(1);
+console.log(p);
