384dd9e09eca83fff973cc0e8c56d7ad19ea42eb
최 혜진(Ella) <ella@optolane.com>
2021-10-19 12:37:37 +0900

    클래스 상속 이해하기

--- part2/052.js
+++ part2/052.js
@@ -1,1 +1,25 @@
 // 클래스 상속 이해하기
+class Chart {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  drawLine() {
+    console.log('draw line');
+  }
+}
+
+class BarChart extends Chart {
+  constructor(width, height) {
+    super(width, height);
+  }
+
+  draw() {
+    this.drawLine();
+    console.log(`draw ${this.width} X ${this.height} barChart`);
+  }
+}
+
+const barchart1 = new BarChart(100, 100);
+barchart1.draw();
