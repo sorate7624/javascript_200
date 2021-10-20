6bbaa6f05a16d4f76f8bfefe8554ac9182dcd733
최 혜진(Ella) <ella@optolane.com>
2021-10-20 12:29:02 +0900

    클래스 정적 메소드와 속성 정의하기

---
+++ part2/053-1.js
@@ -0,0 +1,15 @@
+// 클래스 정적 메소드와 속성 정의하기
+class ProductWithCode {
+  static get CODE_PREFIX() {
+    return "PRODUCT-"
+  }
+
+  constructor(id) {
+    this.id
+    this.code = ProductWithCode.CODE_PREFIX + id;
+  }
+}
+
+const product1 = new ProductWithCode('001');
+console.log(ProductWithCode.CODE_PREFIX);
+console.log(product1.code);
