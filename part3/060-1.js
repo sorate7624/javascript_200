92650a2d488943524c597ec74037fa8ea56f1334
최혜진 <choehyejin@choehyejin-ui-MacBookAir.local>
2021-10-25 22:09:20 +0900

    표준 내장 객체 이해하기

---
+++ part3/060-1.js
@@ -0,0 +1,12 @@
+// 표준 내장 객체 이해하기
+const str1 = '자바스크립트 200제';
+const str2 = new String('자바스크립트 200제');
+
+console.log(typeof str1);
+console.log(typeof str2);
+
+console.log(str1 === '자바스크립트 200제');
+console.log(str2 === '자바스크립트 200제');
+
+console.log(str1.valueOf());
+console.log(str2.valueOf());
