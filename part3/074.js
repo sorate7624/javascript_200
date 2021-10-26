b747584c5ab45ac1e0eae2331515eb653fcdd9cd
최 혜진(Ella) <ella@optolane.com>
2021-10-26 12:33:47 +0900

    특정 위치의 문자 반환하기 (charAt)

--- part3/074.js
+++ part3/074.js
@@ -1,1 +1,8 @@
 // 특정 위치의 문자 반환하기 (charAt)
+const str = 'Good afternoon, Good evening, and Good night!' + '- The Truman Show, 1998';
+console.log(str.charAt(0));
+console.log(str.charAt(5));
+console.log(str.charAt(14));
+console.log(str.length);
+console.log(str.charAt(500));
+
