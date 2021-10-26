761b36567b378c9f79558339030345fc4080ec0b
최 혜진(Ella) <ella@optolane.com>
2021-10-26 12:30:08 +0900

    두 개의 문자열 하나로 합치기 (concat)

--- part3/073.js
+++ part3/073.js
@@ -1,1 +1,6 @@
 // 두 개의 문자열 하나로 합치기 (concat)
+const str1 = 'Good afternoon';
+const str2 = ', Good evening';
+const str3 = ', and Good night!';
+const str4 = ' - The Truman Show, 1998';
+console.log(str1.concat(str2, str3, str4));
