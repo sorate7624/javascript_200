da328a4f06dd0118baa42490aa329617d9e7cbc8
최 혜진(Ella) <ella@optolane.com>
2021-10-27 12:13:02 +0900

    특정 문자열 위치 확인하기 2(lastindexOf)

--- part3/076.js
+++ part3/076.js
@@ -1,1 +1,7 @@
 // 특정 문자열 위치 확인하기 2(lastindexOf)
+const str = 'Carpe diem, seize the day';
+
+console.log(`"e"는 ${str.lastIndexOf('e')} 번째 인덱스에 있습니다.`);
+console.log(`대문자 "C"는 ${str.lastIndexOf('C')} 번째 인덱스에 있습니다.`);
+console.log(`소문자 "c"는 ${str.lastIndexOf('c')} 번째 인덱스에 있습니다.`);
+console.log(`문자열 ", se"는 ${str.lastIndexOf(', se')} 번째 인덱스에 있습니다.`);
