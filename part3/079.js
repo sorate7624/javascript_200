88b5d4959241d8aae2600965b18f3631c46e5442
최 혜진(Ella) <ella@optolane.com>
2021-10-27 12:18:20 +0900

    배열 요소를 분할/변환하기 (from)

--- part3/079.js
+++ part3/079.js
@@ -1,1 +1,8 @@
 // 배열 요소를 분할/변환하기 (from)
+const str = '12345678';
+
+const distributtedArr = Array.from(str);
+console.log(distributtedArr);
+
+const modifiedArr = Array.from(distributtedArr, el => el * 2);
+console.log(modifiedArr);
