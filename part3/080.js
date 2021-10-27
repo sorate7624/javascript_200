2d19d920f5be56642dd2b62ff0d73e34f40b383a
최 혜진(Ella) <ella@optolane.com>
2021-10-27 12:43:02 +0900

    파일 생성 및  파일 내 주석 추가

--- part3/080.js
+++ part3/080.js
@@ -1,1 +1,12 @@
 // 문자열을 특정 구분자에 의해 배열로 나누기 (split)
+const capitals = `Parague, Czech Republic
+Copenhagen,Denmark
+Paris,France,
+Madrid,Spain
+Rome,Italy`
+
+capitals.split('\n').forEach(s => {
+    const capital = s.split(',')[0];
+    const country = s.split(',')[1];
+    console.log(`${capital} is in ${country}`);
+});
