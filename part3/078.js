4edc9fd789abb3421f6f81e5a16497eaed740c29
최 혜진(Ella) <ella@optolane.com>
2021-10-27 12:16:59 +0900

    문자열 대소문자 변환하기 (toLowerCase, toUpperCase)

--- part3/078.js
+++ part3/078.js
@@ -1,1 +1,7 @@
 // 문자열 대소문자 변환하기 (toLowerCase, toUpperCase)
+console.log('Find Your Own Voice'.toLowerCase());
+console.log('Find Your Own Voice'.toUpperCase());
+
+const value = 'Find Your Own Voice';
+console.log(value.toLowerCase() === value.toUpperCase());
+console.log(value.toLowerCase() == value.toUpperCase());
