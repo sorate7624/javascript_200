0a6189d22d7e9137f637a90681322a2038e3a16b
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:59:31 +0900

    배열의 특정 요소 위치 확인하기(indexOf)

--- part3/090.js
+++ part3/090.js
@@ -1,1 +1,5 @@
 // 배열의 특정 요소 위치 확인하기(indexOf)
+const arr = ['spring', 'summer', 'fall', 'winter', 'is', 'down'];
+
+console.log(`"winter" is in this index ${arr.indexOf('winter')}`);
+console.log(`"winter" is not in here, look this value ${arr.indexOf('winter', 4)} `);
