2829860a94b98f6b7eedcf646cb8df5019cf621c
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:05:05 +0900

    배열 뒤에 요소 추가하기(push)

--- part3/081.js
+++ part3/081.js
@@ -1,1 +1,12 @@
 // 배열 뒤에 요소 추가하기(push)
+const festa = ['mang'];
+festa.push('chimmy');
+festa.push('tata');
+festa.push('cooky');
+festa.push('shooky');
+festa.push('koya');
+festa.push('rg');
+
+festa.forEach(name => {
+  console.log(name);
+})
