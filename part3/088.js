99b2f6f021b9e3a3d516d0c2635ac5a1edfedaca
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:23:26 +0900

    배열 특정 위치의 요소 추출하기(slice)

--- part3/088.js
+++ part3/088.js
@@ -1,1 +1,4 @@
 // 배열 특정 위치의 요소 추출하기(slice)
+const arr = ['melon', 'lemon', 'source', 'apple', 'juice'];
+console.log(`과일이 아닌 요소는 ${arr.slice(2,3)}와 ${arr.slice(4,5)} 입니다.`);
+console.log(arr.slice(0, 10));
