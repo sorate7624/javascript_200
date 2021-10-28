e2d773586a2475ed03e6d00d9f3e9a829fae220c
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:11:00 +0900

    배열 길이 구하기(length)

--- part3/083.js
+++ part3/083.js
@@ -1,1 +1,20 @@
 // 배열 길이 구하기(length)
+const ship = {
+  max: 4,
+  passengers: [],
+  onBoard: function (name) {
+    if (this.passengers.length === 4) {
+      console.log(`This ship is full. ${name} can not board this ship`);
+    } else {
+      this.passengers.push(name);
+      console.log(`${name} boarded.`);
+    }
+  }
+}
+
+ship.onBoard('chloe');
+ship.onBoard('jay');
+ship.onBoard('david');
+ship.onBoard('asher');
+ship.onBoard('daniel');
+console.log(ship.passengers);
