a04b76851974f14023dfc4f048f92cab601c536c
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:15:09 +0900

    배열에 특정 구분자 넣어 문자형으로 변환하기(join)

--- part3/085.js
+++ part3/085.js
@@ -1,1 +1,11 @@
 // 배열에 특정 구분자 넣어 문자형으로 변환하기(join)
+const dialogue = [
+  'Fear is the path to the dark side',
+  'Fear leads to anger',
+  'Anger leads to hate',
+  'Hate leads to suffering',
+  'I sense mush fear in you.'
+];
+
+console.log(dialogue.join('. '));
+console.log(dialogue.join(',\n'));
