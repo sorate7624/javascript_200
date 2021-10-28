4153f8c698b5fc75e50139ffd01e86b4138eaec4
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:56:14 +0900

    배열 인덱스로 특정 요소 수정하기(splice)

--- part3/089.js
+++ part3/089.js
@@ -1,1 +1,8 @@
 // 배열 인덱스로 특정 요소 수정하기(splice)
+const fruits = ['melon', 'lemon', 'source', 'apple', 'juice'];
+
+fruits.splice(4, 1);
+fruits.splice(4, 0, 'grape');
+fruits.splice(2, 1, 'mandarin', 'strawberry', 'watermelon');
+
+console.log(fruits);
