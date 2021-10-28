526a4346b42139bcab52fc48c7f8e498de6c212f
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:13:12 +0900

    배열 합치기(concat)

--- part3/084.js
+++ part3/084.js
@@ -1,1 +1,10 @@
 // 배열 합치기(concat)
+const prevList = [1, 2, 3];
+const currentList = [4, 5, 6];
+const nextList = [7, 8, 9];
+
+console.log(prevList.concat('합치기'));
+console.log(prevList.concat(currentList, nextList));
+
+console.log(['배열'].concat('합치기'));
+console.log(['배열'].concat('합치기', 'Javascript200'));
