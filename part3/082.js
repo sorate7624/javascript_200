01c65c0ebaff079d28ece90fcb717dbbbfd08e33
최 혜진(Ella) <ella@optolane.com>
2021-10-28 12:08:27 +0900

    배열 뒤에 요소 추가하기(unshift)

--- part3/082.js
+++ part3/082.js
@@ -1,1 +1,12 @@
 // 배열 뒤에 요소 추가하기(unshift)
+const festa = ['mang'];
+festa.unshift('chimmy');
+festa.unshift('tata');
+festa.unshift('cooky');
+festa.unshift('shooky');
+festa.unshift('koya');
+festa.unshift('rj');
+
+festa.forEach(name => {
+  console.log(name);
+})
