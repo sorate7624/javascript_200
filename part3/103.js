9112bb768c8a78a9e7e45b459f14ebb92cc4929a
최 혜진(Ella) <ella@optolane.com>
2021-10-29 12:45:16 +0900

    객체를 배열로 변환하기(entries)

--- part3/103.js
+++ part3/103.js
@@ -1,1 +1,10 @@
 // 객체를 배열로 변환하기(entries)
+const obj = {
+  movie: 'sunny',
+  music: 'Like sugar',
+  style: 'retro',
+  price: 'Infinity'
+};
+
+const modifiedObj = Object.entries(obj);
+console.log(modifiedObj);
