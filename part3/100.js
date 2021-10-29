388b2e17e10fbbe94613468fb0ec12a3111279a2
최 혜진(Ella) <ella@optolane.com>
2021-10-29 12:30:31 +0900

    중첩된 배열을 단일하게 만들기(reduce)

--- part3/100.js
+++ part3/100.js
@@ -1,1 +1,11 @@
 // 중첩된 배열을 단일하게 만들기(reduce)
+const arr = [1, [2, 3],
+  [4, 5, 6],
+  ['배열', '나열하기'], 'Javascript'
+];
+
+const result = arr.reduce((acc, el) => {
+  return acc.concat(el);
+}, []);
+
+console.log(result);
