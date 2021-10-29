f4422d7d8c4a2c43d11456d0a163b5fe4dd81a09
최 혜진(Ella) <ella@optolane.com>
2021-10-29 12:05:04 +0900

    배열 순환하기(forEach)

--- part3/091.js
+++ part3/091.js
@@ -1,1 +1,26 @@
 // 배열 순환하기(forEach)
+const arr = [{
+    id: 0,
+    name: '혜림',
+    age: 6
+  },
+  {
+    id: 1,
+    name: '현일',
+    age: 3
+  },
+  {
+    id: 2,
+    name: '현아',
+    age: 5
+  },
+  {
+    id: 3,
+    name: '우림',
+    age: 2
+  }
+];
+
+arr.forEach((el) => {
+  console.log(el.name);
+});
