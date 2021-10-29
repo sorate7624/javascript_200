a7dc068dd6aeb14a2c5563583fdd1320d6d1638c
최 혜진(Ella) <ella@optolane.com>
2021-10-29 12:53:34 +0900

    객체에 속성 추가 못하게 만들기(seal)

--- part3/105.js
+++ part3/105.js
@@ -1,1 +1,17 @@
 // 객체에 속성 추가 못하게 만들기(seal)
+const album = {
+  name: 'LOVE YOURSELF'
+};
+
+album.song = 'Euporia';
+album.singer = 'RM';
+
+console.log(album);
+
+Object.seal(album)
+
+album.comment = 'Answer';
+album.singer = 'JK';
+delete album.name;
+
+console.log(album);
