2901e8d89b0afa8828ff666ac37b108b0059f11a
최 혜진(Ella) <ella@optolane.com>
2021-10-29 12:22:20 +0900

    배열의 특정 조건을 충족하는 요소 찾기(find)

--- part3/097.js
+++ part3/097.js
@@ -1,1 +1,30 @@
 // 배열의 특정 조건을 충족하는 요소 찾기(find)
+const arr = [{
+    id: 0,
+    name: '혜림',
+    age: 6
+  },
+  {
+    id: 1,
+    name: '현일',
+    age: 3
+  },
+  {
+    id: 1,
+    name: '탄이',
+    age: 30
+  },
+  {
+    id: 2,
+    name: '현아',
+    age: 5
+  },
+  {
+    id: 3,
+    name: '우림',
+    age: 2
+  }
+];
+
+const myFriend = arr.find(a => a.age === 30);
+console.log(myFriend);
