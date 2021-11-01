ceb8082ea63b2e0695200d47f0370c37ec5980f0
최 혜진(Ella) <ella@optolane.com>
2021-11-01 12:57:29 +0900

    특정 자리수에 반올림하기(round)

--- part3/110.js
+++ part3/110.js
@@ -1,1 +1,8 @@
 // 특정 자리수에 반올림하기(round)
+const val = 573.926;
+
+console.log(Math.round(val));
+console.log(Math.round(val * 10) / 10);
+console.log(Math.round(val * 100) / 100);
+console.log(Math.round(val / 100) * 10);
+console.log(Math.round(val / 100) * 100);
