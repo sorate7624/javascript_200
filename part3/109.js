cb9a5f6211a808e2e2fe2d9c2b96bda59fcb3af9
최 혜진(Ella) <ella@optolane.com>
2021-11-01 12:47:40 +0900

    랜덤값 구하기(random)

--- part3/109.js
+++ part3/109.js
@@ -1,1 +1,12 @@
 // 랜덤값 구하기(random)
+const generateRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(generateRandom(1, 10));
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(generateRandom(10, 100));
+}
