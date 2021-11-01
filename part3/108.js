5485a271906db7d807030b11fb25350cfffdfa20
최 혜진(Ella) <ella@optolane.com>
2021-11-01 12:44:54 +0900

    10진수 아닌 진법을 다른 진법으로 변환하기(parseInt)

--- part3/108.js
+++ part3/108.js
@@ -1,1 +1,14 @@
 // 10진수 아닌 진법을 다른 진법으로 변환하기(parseInt)
+const bin = 1000010011;
+const oct = 1023;
+const hex = 213;
+
+const dexByBin = parseInt(bin, 2);
+const dexByOct = parseInt(bin, 8);
+const dexByHex = parseInt(bin, 16);
+const hexByOct = parseInt(bin, 8).toString(16);
+
+console.log(dexByBin);
+console.log(dexByOct);
+console.log(dexByHex);
+console.log(hexByOct);
