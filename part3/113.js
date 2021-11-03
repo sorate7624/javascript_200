150fb62d5d29721f90653a175b9db6097f5c00c1
최 혜진(Ella) <ella@optolane.com>
2021-11-03 12:52:42 +0900

    현재 시간을 원하는 포맷으로 출력하기(getFullYear/getMonth/getDate)

--- part3/113.js
+++ part3/113.js
@@ -1,1 +1,10 @@
 // 현재 시간을 원하는 포맷으로 출력하기(getFullYear/getMonth/getDate)
+Date.prototype.yyyymmdd = function () {
+  const yyyy = this.getFullYear();
+  const mm = this.getMonth() < 9 ? `0${this.getMonth()+1}` : this.getMonth() + 1;
+  const dd = this.getDate() < 10 ? `0${this.getDate()}` : this.getDate();
+  return '' + yyyy + mm + dd;
+}
+
+const date = new Date();
+console.log(date.yyyymmdd());
