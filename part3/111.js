523ce7d5a3415b47fd80117db5318a60f28b00da
최 혜진(Ella) <ella@optolane.com>
2021-11-03 12:48:21 +0900

    특정 자리수에서 올림하기(ceil)

--- part3/111.js
+++ part3/111.js
@@ -1,1 +1,10 @@
 // 특정 자리수에서 올림하기(ceil)
+const positiveNum = 93.54;
+const negativeNum = -39.27;
+
+console.log(Math.ceil(positiveNum));
+console.log(Math.ceil(negativeNum));
+console.log(Math.ceil(positiveNum * 10) / 10);
+console.log(Math.ceil(positiveNum / 10) * 10);
+console.log(Math.ceil(negativeNum * 10) / 10);
+console.log(Math.ceil(negativeNum / 10 * 10));
