f78996b95f6d2560750d4a11f4e6b2badcd96cd6
최 혜진(Ella) <ella@optolane.com>
2021-11-03 12:58:37 +0900

    UTC 기준 날짜 출력하기(Date, UTC)

--- part3/114.js
+++ part3/114.js
@@ -1,1 +1,5 @@
 // UTC 기준 날짜 출력하기(Date, UTC)
+const date = new Date();
+const dateUTC = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+
+console.log(new Date(dateUTC));
