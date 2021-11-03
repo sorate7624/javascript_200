8715db41f7913c984a64df0d71cc2e2931ea5af8
최 혜진(Ella) <ella@optolane.com>
2021-11-03 12:50:31 +0900

    특정 자리수에서 내림하기(floor)

--- part3/112.js
+++ part3/112.js
@@ -1,1 +1,10 @@
 // 특정 자리수에서 내림하기(floor)
+const positiveNum = 93.54;
+const negativeNum = -39.27;
+
+console.log(Math.floor(positiveNum));
+console.log(Math.floor(negativeNum));
+console.log(Math.floor(positiveNum * 10) / 10);
+console.log(Math.floor(positiveNum / 10) * 10);
+console.log(Math.floor(negativeNum * 10) / 10);
+console.log(Math.floor(negativeNum / 10) * 10);
