9374e85c951e99b28695388392384e303b3ec092
최 혜진(Ella) <ella@optolane.com>
2021-11-05 12:36:31 +0900

    배열 순회하기(for-of)

--- part3/125.js
+++ part3/125.js
@@ -1,1 +1,15 @@
 // 배열 순회하기(for-of)
+const products = [{
+  name: '가방'
+}, {
+  name: '노트북'
+}];
+
+for (const item of products) {
+  console.log(item.name);
+}
+
+const iter = products[Symbol.iterator]();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
