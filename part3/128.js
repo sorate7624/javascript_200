4b8a2b32984f22fe5c19ad3c736e8ef58551d3c4
최 혜진(Ella) <ella@optolane.com>
2021-11-05 12:46:06 +0900

    Map 객체 요소 나열하기(keys, value, entries)

--- part3/128.js
+++ part3/128.js
@@ -1,1 +1,19 @@
 // Map 객체 요소 나열하기(keys, value, entries)
+const map = new Map();
+
+map.set('one', 1);
+map.set('two', 2);
+map.set('three', 3);
+
+const keys = map.keys();
+const values = map.values();
+const entries = map.entries();
+
+
+console.log(keys.next().value);
+console.log(values.next().value);
+console.log(entries.next().value);
+
+console.log(keys);
+console.log(values);
+console.log(entries);
