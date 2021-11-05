b02e84c85788be49340f2d05e6b53b6f57f14aa0
최 혜진(Ella) <ella@optolane.com>
2021-11-05 12:38:06 +0900

    Map 객체에 요소 추가/삭제/확인하기(set, get, delete, has)

--- part3/126.js
+++ part3/126.js
@@ -1,1 +1,12 @@
 // Map 객체에 요소 추가/삭제/확인하기(set, get, delete, has)
+const map = new Map();
+
+map.set('one', 1);
+map.set('two', 2);
+
+console.log(map.get('one'));
+console.log(map.has('one'));
+map.delete('one');
+
+console.log(map.has('one'));
+console.log(map.has('two'));
