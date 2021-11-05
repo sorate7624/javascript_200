8cfbd8078a2b4e80a272cdc15f21b7d186f251f2
최 혜진(Ella) <ella@optolane.com>
2021-11-05 12:18:39 +0900

    정규표현식으로 문자열 변환하기(exec)

--- part3/121.js
+++ part3/121.js
@@ -1,1 +1,13 @@
-// 정규표현식으로 문자열 변환학(exec)
+// 정규표현식으로 문자열 변환하기(exec)
+const str = 'Java is not in Javascript';
+
+const result1 = /java/ig.exec(str);
+console.log(result1[0]);
+console.log(result1.index);
+console.log(result1.input);
+
+const nums = '"1","2","3"';
+const regex = /\d+/g;
+while (result2 = regex.exec(nums)) {
+  console.log(result2, regex.lastIndex);
+}
