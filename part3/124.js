e2f38e144e513efed209bf7822d6b9492dded9c7
최 혜진(Ella) <ella@optolane.com>
2021-11-05 12:32:53 +0900

    문자열 순회하기(for-of)

--- part3/124.js
+++ part3/124.js
@@ -1,1 +1,14 @@
 // 문자열 순회하기(for-of)
+const str = "hello";
+
+for (const item of str) {
+  console.log(item);
+}
+
+const iter = str[Symbol.iterator]();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
