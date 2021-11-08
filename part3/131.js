6f8280c72ba56edc6b3dfa5b7b4da230a68e592b
최 혜진(Ella) <ella@optolane.com>
2021-11-08 12:04:37 +0900

    Set 객체의 값 추가/삭제/확인하기(add, delete, has)

--- part3/131.js
+++ part3/131.js
@@ -1,1 +1,11 @@
 // Set 객체의 값 추가/삭제/확인하기(add, delete, has)
+const s = new Set();
+
+s.add('one');
+s.add('two');
+s.add('three');
+
+console.log(s.has('one'));
+s.delete('one');
+console.log(s.has('one'));
+console.log(s.has('two'));
