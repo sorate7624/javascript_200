c61388b0f88a05d8dd18c2dce7f8f21660d873b2
최 혜진(Ella) <ella@optolane.com>
2021-11-08 12:29:23 +0900

    Promise 이해하기

--- part3/138.js
+++ part3/138.js
@@ -1,1 +1,24 @@
 // Promise 이해하기
+function promiseFoHomework(mustDo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('doing homework');
+      if (mustDo) {
+        resolve({
+          result: 'homework-result'
+        });
+      } else {
+        reject(new Error('Too lazy!'));
+      }
+    }, 3000);
+  });
+}
+
+const promiseA = promiseFoHomework(true);
+console.log('promiseA created');
+
+const promiseB = promiseFoHomework();
+console.log('promiseB created');
+
+promiseA.then(v => console.log(v));
+promiseA.then(v => console.log(v).catch(e => console.log(e)));
