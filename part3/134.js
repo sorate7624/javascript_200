a451b50ea8d713380fcdf8ad5d3727a52775204d
최 혜진(Ella) <ella@optolane.com>
2021-11-08 12:11:09 +0900

    Set 객체 값 나열하기(keys, value, entries)

--- part3/134.js
+++ part3/134.js
@@ -1,1 +1,18 @@
 // Set 객체 값 나열하기(keys, value, entries)
+const s = new Set();
+
+s.add('one');
+s.add('two');
+s.add('three');
+
+const keys = s.keys();
+const values = s.values();
+const entries = s.entries();
+
+console.log(keys.next().value);
+console.log(values.next().value);
+console.log(entries.next().value);
+
+console.log(keys);
+console.log(values);
+console.log(entries);
