a691b44e252df727f1de62ba5dbbcc9b1d6dcb8a
최 혜진(Ella) <ella@optolane.com>
2021-11-08 12:06:21 +0900

    Set 객체의 크기 확인하기(size)

--- part3/132.js
+++ part3/132.js
@@ -1,1 +1,15 @@
-// Set 객체의 크기 확인하기(sie)
+// Set 객체의 크기 확인하기(size)
+const s = new Set();
+
+s.add('one');
+s.add(2);
+s.add([1, 2, 3]);
+s.add({
+  a: 'A',
+  b: 'B'
+});
+s.add(function () {});
+
+console.log(s.size);
+s.clear();
+console.log(s.size);
