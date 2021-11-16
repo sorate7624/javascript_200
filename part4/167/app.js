62b0a1c31c2d3d6b3a0c5284a1699139e11cd96a
최 혜진(Ella) <ella@optolane.com>
2021-11-16 12:35:02 +0900

    Event Emitter 이해하기 1

--- part4/167/app.js
+++ part4/167/app.js
@@ -1,1 +1,13 @@
 // Event Emitter 이해하기 1
+const Emitter = require('./Emitter');
+const em = new Emitter();
+
+em.on('greet', () => {
+  console.log('Hello First');
+});
+
+em.on('greet', () => {
+  console.log('Hello Second');
+});
+
+em.emit('greet');
