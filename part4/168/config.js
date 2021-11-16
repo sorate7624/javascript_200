ff6afaa5b3f0e21fe009b8467c318534ea5ac80a
최 혜진(Ella) <ella@optolane.com>
2021-11-16 12:40:59 +0900

    Event Emitter 이해하기 2

--- part4/168/config.js
+++ part4/168/config.js
@@ -1,1 +1,6 @@
 // Event Emitter 이해하기 2
+module.exports = {
+  events: {
+    GREET: 'greet'
+  }
+}
--- part4/168/eventEmitter.js
+++ part4/168/eventEmitter.js
@@ -1,1 +1,14 @@
 // Event Emitter 이해하기 2
+const Emitter = require('events');
+const evenetConfig = require('./config').events;
+const em = new Emitter();
+
+em.on(evenetConfig.GREET, () => {
+  console.log('Somewhere, someone said hello.');
+});
+
+em.on(evenetConfig.GREET, () => {
+  console.log('A Greeting occrred!');
+});
+
+em.emit(evenetConfig.GREET)
