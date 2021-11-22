03e03f7eb20b022fc5a91104d5a62c149cef95de
최 혜진(Ella) <ella@optolane.com>
2021-11-22 12:59:52 +0900

    웹 API 작성하기 2

--- part4/177.js
+++ part4/177.js
@@ -1,1 +1,36 @@
 // 웹 API 작성하기 2
+"use strict"
+
+const http = require('http');
+const qs = require('querystring');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'POST':
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        const obj = qs.parse(body);
+        res.writeHead(200);
+        res.end(JSON.stringify(obj));
+      });
+
+      req.on('error', (err) => {
+        console.log(err.stack);
+      });
+      break
+    default:
+      res.end()
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
