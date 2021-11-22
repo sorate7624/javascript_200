0cad2975a9b79028355b8db03fc555f6599c7471
최 혜진(Ella) <ella@optolane.com>
2021-11-22 12:25:46 +0900

    특정 폴더 내 모든 파일 삭제하기

--- part4/174.js
+++ part4/174.js
@@ -1,1 +1,49 @@
 // 특정 폴더 내 모든 파일 삭제하기
+/* 실행 주의
+"use strict"
+
+const fs = require('fs');
+const path = require('path');
+
+const removePath = (p, callback) => {
+  fs.stat(p, (err, stats) => {
+    if (err) {
+      return callback(err);
+    }
+
+    if (!stats.isDirectory()) {
+      console.log('이 경로는 파일입니다');
+      return fs.unlink(err => err ? callback(err) : callback(null, p));
+    }
+
+    console.log('이 경로는 폴더입니다');
+
+    fs.rmdir(p, (err) => {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, p);
+    });
+  });
+}
+
+const printResult = (err, result) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`${result}를 정상적으로 삭제했습니다`);
+}
+
+const p = path.join(__dirname, '/');
+
+try {
+  const files = fs.readdirSync(p);
+  if (files.length) {
+    files.forEach(f => removePath(path.join(p, f), printResult));
+  }
+} catch (err) {
+  if (err) {
+    return console.log(err);
+  }
+}
+*/
