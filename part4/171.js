3ed82d7d35a55c2632055186a495f26abe2169c0
최 혜진(Ella) <ella@optolane.com>
2021-11-22 12:12:31 +0900

    파일 정보 탐색하기

--- part4/171.js
+++ part4/171.js
@@ -1,1 +1,41 @@
-// 파이 정보 탐색하기
+// 파일 정보 탐색하기
+"use strict"
+
+const fs = require('fs');
+const path = require('path');
+
+const fileName = 'hello.txt';
+const targetPath = path.join(__dirname, '/');
+
+const filePath = path.join(targetPath, fileName);
+console.log(path.parse(filePath));
+
+const isFileInPath = (path, fileName, callback) => {
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      return callback(err);
+    }
+
+    let isHere = false;
+    files.forEach(f => {
+      if (f === fileName) {
+        isHere = true;
+      }
+    })
+    return callback(null, isHere);
+  });
+}
+
+isFileInPath(path.join(__dirname, '/'), fileName, (err, isTrue) => {
+  if (err || !isTrue) {
+    return console.log('파일을 읽을 수 없습니다');
+  }
+
+  fs.stat(filePath, (err, fileInfo) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log(fileInfo);
+  });
+
+});
