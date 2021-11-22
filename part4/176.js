8a36f851094d42645ea3ec5612359e78dac99ff6
최 혜진(Ella) <ella@optolane.com>
2021-11-22 12:46:02 +0900

    웹 API 작성하기 1

--- part4/176.js
+++ part4/176.js
@@ -1,1 +1,38 @@
 // 웹 API 작성하기 1
+"use strict"
+
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/plain');
+        res.writeHead(200);
+        res.end('Hello! Node.js HTTP Server');
+      } else if (req.url.substring(0, 5) === '/data') {
+        const queryParams = url.parse(req.url, true).query;
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead(200);
+        res.write('<html><head><title>Javascript 200제</title></head>');
+
+        for (let key in queryParams) {
+          res.write(`<h1>${key}</h1>`);
+          res.write(`<h2>${queryParams[ky]}</h2>`);
+        }
+        res.end('</body></html>');
+      }
+      break
+    default:
+      res.end()
+
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
