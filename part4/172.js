7c91d9bc5b40a514a353fdd1111d2b3371bc6fc9
최 혜진(Ella) <ella@optolane.com>
2021-11-22 12:12:59 +0900

    파일 읽기

--- part4/172.js
+++ part4/172.js
@@ -1,1 +1,34 @@
 // 파일 읽기
+"use strict"
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '/', 'hello.txt');
+
+fs.open(filePath, (err, fd) => {
+  if (err && err.code === 'ENOENT') {
+    return console.log('읽을 수 없는 파일입니다');
+  }
+  
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('파일을 정상적으로 읽을 수 있습니다');
+
+
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+});
